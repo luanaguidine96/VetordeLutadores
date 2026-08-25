378258dc90189953ade18830a38642318f42bcb8
Luana Guidine <luanaguidine96@gmail.com>
2026-08-25 09:26:21 -0300

    first commit

---
+++ Luta.java
@@ -0,0 +1,83 @@
+import java.util.Random;
+public class Luta {
+    private Lutador desafiado ;
+    private Lutador desafiante ;
+    private int rounds;
+    private boolean aprovada;
+
+    public void marcarLuta(Lutador l1, Lutador l2){
+    if (l1.getCategoria() .equals (l2.getCategoria())  && l1 != l2){
+        this.aprovada = true;
+        this.desafiado = l1;
+        this.desafiante = l2;
+    } else {
+        this.aprovada = false;
+        this.desafiado = null;
+        this.desafiante = null;
+    }
+    }
+    public void lutar(){
+    if (this.aprovada){
+        System.out.println("### DESAFIADO ###");
+        this.desafiado.apresentar();
+        System.out.println("### DESAFIANTE ###");
+        this.desafiante.apresentar();
+
+        Random aleatorio = new Random();
+        int vencedor = aleatorio.nextInt(3); // 0 1 2
+        switch (vencedor){
+            case 0: // empate
+                System.out.println("Empatou!");
+                this.desafiado.empatarLuta();
+                this.desafiante.empatarLuta();
+                break;
+            case 1: // desafiado vence
+                System.out.println("Vitória do " + this.desafiado.getNome());
+                this.desafiado.ganharLuta();
+                this.desafiante.perderLuta();
+                break;
+            case 2: // desafiante vence
+                System.out.println("Vitória do " + this.desafiante.getNome());
+                this.desafiado.perderLuta();
+                this.desafiante.ganharLuta();
+                break;
+        }
+    
+    } else {
+        System.out.println("A luta não pode acontecer!");
+    }
+    }
+
+
+public void setDesafiado(Lutador dd) {
+        this.desafiado = dd;
+    }
+    public void setDesafiante(Lutador dt) {
+        this.desafiante = dt;
+    }
+    public void setRounds(int r) {
+        this.rounds = r;
+    }
+    public void setAprovada(boolean a) {
+        this.aprovada = a;
+    }
+
+
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public boolean getAprovada() {
+        return aprovada;
+    }
+}
+
